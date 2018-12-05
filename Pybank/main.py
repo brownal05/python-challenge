09dcb5d3b2091fc1156102a2a24f2b9385b5b274
JayhawkinTex <alex.brown.f@gmail.com>
2018-12-05 17:43:55 -0600

    trash

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,14 +1,18 @@
 import os
 import csv
 
-bank_file = os.path.join('..\Resources\budget_data.csv')
-
-with open(bank_file, newline='') as csvfile:
+csvpath = os.path.join("", "Resources", "budget_data.csv")
+print(csvpath)
+# 
+with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")     
-    for row in csvreader:
-        print(row)
- 
-    
+   # print(f"CSV Header: {csv_header}")     
+    for months in csvreader:
+        row_count =  
+       #sum(1 for months in csvfile))
+        print("Number of months: " + str(row_count)) 
+#    yr_pl_sum = sum(row[1])
+#    print("Total Profit/Loss: " + str(yr_pl_sum)) 
+
 
