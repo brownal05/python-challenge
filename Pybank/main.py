8f6487bf4e40530a860b91d27dc681dc6d0576c8
Alexander Brown <alex.brown.f@gmail.com>
2018-12-04 22:32:43 -0600

    sums

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,14 +1,14 @@
 import os
 import csv
 
-bank_file = os.path.join('..\Resources\budget_data.csv')
-
+bank_file = os.path.join("", "Resources", "budget_data.csv")
 with open(bank_file, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")     
-    for row in csvreader:
-        print(row)
- 
-    
+    #print(f"CSV Header: {csv_header}")    
+    row_count = sum(1 for row in csvfile) 
+    #print(sum(int(x[1]) for x in csvreader))
+    value_sum = sum([row[1] for row in csvreader])
+print(row_count)
+print(int(value_sum)) 
 
