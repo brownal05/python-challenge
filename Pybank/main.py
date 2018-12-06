df52cd7059501512b8d1f01d8af383d02ee17707
JayhawkinTex <alex.brown.f@gmail.com>
2018-12-05 18:33:09 -0600

    still averages

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,18 +1,38 @@
 import os
 import csv
 
-csvpath = os.path.join("", "Resources", "budget_data.csv")
-print(csvpath)
-# 
-with open(csvpath, newline='') as csvfile:
+#open csv
+bank_file = os.path.join("", "Resources", "budget_data.csv")
+Change = []
+
+MyList = []
+#count the rows
+with open(bank_file, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-   # print(f"CSV Header: {csv_header}")     
-    for months in csvreader:
-        row_count =  
-       #sum(1 for months in csvfile))
-        print("Number of months: " + str(row_count)) 
-#    yr_pl_sum = sum(row[1])
-#    print("Total Profit/Loss: " + str(yr_pl_sum)) 
+    #print(f"CSV Header: {csv_header}")    
+    RowCount = sum(1 for row in csvfile) 
+#P/L calculation
+with open(bank_file, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    BankList = list(csvreader)
+    #print(sum(int(x[1]) for x in csvreader))
+    Profits = sum(int(row[1]) for row in BankList)
+#Avg change between months
+with open(bank_file, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    BankList = list(csvreader)   
+    PrevRev = 0
 
-
+    for row in BankList:
+        
+        Change =  int(row[1]) - PrevRev 
+        PrevRev = int(row[1])
+        MyList.append(Change)
+        
+print(MyList)
+print(RowCount)
+print(str(Profits))
+print(str(Change))
