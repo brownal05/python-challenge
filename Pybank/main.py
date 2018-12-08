89bc74c407db04334084e7d74044adaf2c069f23
JayhawkinTex <alex.brown.f@gmail.com>
2018-12-08 11:00:04 -0600

    syntax error, missing one line

--- Pybank/main.py
+++ Pybank/main.py
@@ -3,36 +3,30 @@ import csv
 
 #open csv
 bank_file = os.path.join("", "Resources", "budget_data.csv")
-Change = []
+pnlChange = []
+pnl = []
+date = []
 
-MyList = []
-#count the rows
-with open(bank_file, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")    
-    RowCount = sum(1 for row in csvfile) 
-#P/L calculation
-with open(bank_file, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader)
-    BankList = list(csvreader)
-    #print(sum(int(x[1]) for x in csvreader))
-    Profits = sum(int(row[1]) for row in BankList)
-#Avg change between months
-with open(bank_file, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    csv_header = next(csvreader)
-    BankList = list(csvreader)   
-    PrevRev = 0
 
-    for row in BankList:
+with open(bank_file, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    next(csvreader)
+    #print(f"CSV Header: {csv_header}") 
+    for row in csvreader:
         
-        Change =  int(row[1]) - PrevRev 
-        PrevRev = int(row[1])
-        MyList.append(Change)
-        
-print(MyList)
-print(RowCount)
-print(str(Profits))
-print(str(Change))
+        date.append(row[0])
+        pnl.append(float(row[1]))
+    for i in range(1, len(pnl)):
+        pnlChange.append(pnl[i] - pnl[i-1])
+        avgPnlChange = sum(pnlChange)/len(pnlChange)
+        maxPnlChange = max(pnlChange)
+        minPnlChange = min(pnlChange)
+        maxPnlChangeDate = str(date[pnlChange.index(max(pnlChange))])
+        minPnlChangeDate = str(date[pnlChange.index(max(pnlChange))])
+
+print("Financial Analysis")
+print("----------------------------")
+print("Total Months: ", len(date))
+print("Total: $", sum(pnl))
+print("Average Change: $", avgPnlChange)
